48a91459d250a6550804c5246a59bc344c717214
Jonjoe Whitfield <me@jonjoe.io>
2020-04-07 15:21:38 +0100

    stories

--- stage-4/code/src/components/atoms/Input/Input.stories.tsx
+++ stage-4/code/src/components/atoms/Input/Input.stories.tsx
@@ -4,7 +4,6 @@ import { storiesOf } from "@storybook/react";
 
 import Input from "./Input.component";
 
-storiesOf("ColorButton", module)
-    .add("default",
-         () => <Input onChange={() => null} value="hello"/>
-    )
+storiesOf("<Input />", module).add("default", () => (
+  <Input onChange={() => null} value="hello" />
+));
