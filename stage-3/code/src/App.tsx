3f31890d27065d6604e978af22bf89f6e180333c
Jonjoe Whitfield <me@jonjoe.io>
2020-04-07 14:39:44 +0100

    documentation

--- stage-3/code/src/App.tsx
+++ stage-3/code/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+// Everything imported from a centeral registry
 import { Input, Loader, Page, List } from "./components";
 
 async function asyncGetRepos() {
@@ -28,17 +29,22 @@ function App() {
     });
   }, []);
 
+  // Logic abstracted away from presentational components. The list renderes a list and thats it. 
   const filteredRepos: any = repos.filter((repo: any) =>
     repo.name.includes(searchTerm)
   );
 
   return (
+    /* Page is a Template component responsible for macro layout */
     <Page>
+      {/* Input is an Atom becuase its a single input field. If we added complexity to it then it would be promoted to Molecule.*/}
       <Input
         onChange={inputValue => setSearchTerm(inputValue)}
         value={searchTerm}
       />
 
+      {/* Example of the only kind of logic allowed in the render */}
+      {/* if we abstracted this away we would lose the structure of the component and the complexity will cause problems.*/}
       {reposLoading ? <Loader /> : <List data={filteredRepos} />}
     </Page>
   );
